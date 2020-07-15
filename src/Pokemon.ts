ac3373c746dbc2ef6045da20db6b6ad1a183ba40
Murilo Pereira <mrex@tuta.io>
2020-07-15 11:41:17 -0300

    feat(init): Criado o bot

---
+++ src/Pokemon.ts
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import fs from 'fs';
+class Pokemon {
+    private _nome: string = '';
+    private _imgUrl: string = '';
+    private _tipo: Array<string> = [];
+    private _peso: number = 0;
+    private _index: number = 0;
+
+    constructor(index?: number, nome?: string) {
+        if(index != undefined)
+            this._index = index;
+        
+        if(nome != undefined)
+            this._nome = nome;
+    }
+
+    public getNome(): string {
+        return this._nome;
+    }
+
+    public getImg(): string {
+        return this._imgUrl;
+    }
+
+    public getTipo(): Array<string> {
+        return this._tipo;
+    }
+
+    public getPeso(): number {
+        return this._peso;
+    }
+
+    public getIndex(): number {
+        return this._index;
+    }
+
+    public async fetchData() {
+        let endpoint = `https://pokeapi.co/api/v2/pokemon/${this._index != 0 ? this._index : this._nome.toLowerCase()}`;
+        let pokeResponse = null;
+
+        try {
+            pokeResponse = await axios.get(endpoint);
+            
+            for(let i = 0; i < pokeResponse.data['types'].length; i++) {
+                this._tipo.push(pokeResponse.data['types'][i]['type']['name']);
+            }
+
+            this._nome = pokeResponse.data['name'];
+            this._index = pokeResponse.data['id'];
+            this._peso = pokeResponse.data['weight'];
+            let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokeResponse.data['id'].toString().padStart(3, '0')}.png`
+            let imgUri = `./poke/${pokeResponse.data['id'].toString().padStart(3, '0')}.png`;
+            this._imgUrl = imgUri;
+            if (!fs.existsSync(imgUri)) {
+                console.log(imgUri);
+                await this.downloadImage(imgUrl, imgUri);
+            }            
+
+        
+        } catch(UnhandledPromiseRejectionWarning) {
+            pokeResponse = undefined;
+            this._index = -1;
+        }
+    }
+
+    public getInfo(): string {
+        let info = ''+
+        `*Pokémon:* ${this._nome[0].toUpperCase() + this._nome.slice(1)}\n` +
+        `*Pokédex:* ${this._index}\n` +
+        `*Tipo(s):* ${this._tipo.join(', ')}\n` +
+        `*Peso:* ${this._peso}lbs`;
+        
+        return info;
+    }
+
+    private async downloadImage (url: string, path: string) {  
+        const writer = fs.createWriteStream(path)
+        
+        const response = await axios({
+            url,
+            method: 'GET',
+            responseType: 'stream'
+        })
+        
+        response.data.pipe(writer)
+        
+        return new Promise((resolve, reject) => {
+            writer.on('finish', resolve)
+            writer.on('error', reject)
+        })
+    }
+}
+
+export default Pokemon;
---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import Pokemon from './Pokemon';
+
+const venom = require('venom-bot');
+
+venom.create(
+  "masterClient", (base64Qr: string, asciiQR: string) => {
+    console.log(`\n${asciiQR}`);
+  }, (statusFind:any) => {
+    console.log(statusFind);
+  },{
+    headless: true, // Headless chrome
+    devtools: false, // Open devtools by default
+    useChrome: false, // If false will use Chromium instance
+    debug: false, // Opens a debug session
+    logQR: true, // Logs QR automatically in terminal
+    browserArgs: ['--disable-web-security','--no-sandbox'], // Parameters to be added into the chrome browser instance
+    refreshQR: 15, // Will refresh QR every 15 seconds, 0 will load QR once. Default is 30 seconds
+    autoClose: false, // Will auto close automatically if not synced, 'false' won't auto close. Default is 60 seconds (#Important!!! Will automatically set 'refreshQR' to 1000#)
+    disableSpins: false, // Will disable Spinnies animation, useful for containers (docker) for a better log
+  }
+).then((client: any) => start(client));
+ 
+function start(client: any) {
+  client.onMessage((message: any) => {
+    let messageBody: string = message.body;
+
+    if(messageBody.includes('!pkn')) {
+        let info = messageBody.split(' ');
+        let index = undefined;
+        let nome = undefined;
+        
+        isNaN(Number(info[1])) ? nome = info[1] : index = Number(info[1])
+
+        let pokemon = new Pokemon(index, nome);
+        pokemon.fetchData().then(() => {
+            let info = pokemon.getInfo();
+            let image = pokemon.getImg();
+            client.sendImage(message.from, image, 'img.png', info);
+        });
+    }
+  });
+}
